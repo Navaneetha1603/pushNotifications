6f8832be8e939edda2ef1812b30db1d1e7343f81
Navaneetha1603 <navaneethakandlapelli@gmail.com>
2021-01-13 13:12:18 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pushNotifications';
+  constructor(private swPush:SwPush){}
+  readonly VAPID_PUBLIC_KEY="BAcCa8QHyIi8rx51A4JSBS225Ur7OFP89C7i_9sEziM2gAPfDlaHfIYbogvOhK6wrrnulOfPZFX8Ih2qpUiEl5g";
+  subscribeToPushNotifications(){
+     this.swPush.requestSubscription({
+       serverPublicKey:this.VAPID_PUBLIC_KEY
+     })
+     .then()
+  }
 }
